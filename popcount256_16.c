8106705475c3a655963937bf089706e815887266
Pierre Karpman <pierre.karpman@univ-grenoble-alpes.fr>
2019-09-24 09:30:13 +0200

    small popcount cleanup

--- popcount256_16.c
+++ popcount256_16.c
@@ -8,29 +8,13 @@
 #ifdef VECT
 
 /* Hamming weight of v as a vector of 16x16 */
-int popcount256_16_deac(__m256i v)
-{
-	__m256i zero, live;
-
-	zero = _mm256_setzero_si256();
-
-	live = _mm256_cmpgt_epi16(v, zero);
-	live = _mm256_srli_epi16(live, 15);
-
-	return	  __builtin_popcountl(_mm256_extract_epi64(live,0))
-			+ __builtin_popcountl(_mm256_extract_epi64(live,1))
-			+ __builtin_popcountl(_mm256_extract_epi64(live,2))
-			+ __builtin_popcountl(_mm256_extract_epi64(live,3)); // last one may be omitted if vector's only partially filled
-}
 
 #if defined(__AVX512VL__) && defined(__AVX512BW__)
 // Probably the best?
-// Requires AVX512VL + AVX512BW
 int popcount256_16(__m256i v)
 {
 	return __builtin_popcountl(_mm256_cmpgt_epi16_mask(v, _mm256_setzero_si256()));
 }
-
 #elif defined(__AVX2__)
 int popcount256_16(__m256i v)
 {
@@ -51,25 +35,6 @@ int popcount256_16(__m256i v)
 
 #else
 #error AVX2 or AVX512 extensions needed
-
 #endif
 
-
-
-//int main(int argc, char **argv)
-//{
-//	uint16_t t[16];
-//
-//	for (int i = 0; i < 16; i++)
-//		t[i] = i+4690;
-//	t[14] = 0;
-//	t[15] = 0;
-//	t[12] = 0;
-//
-//	__m256i v = _mm256_loadu_si256((__m256i*)t);
-//
-//	printf("%d\n", popcount256_16_ter(v));
-//
-//	return 0;
-//}
 #endif /* VECT */
